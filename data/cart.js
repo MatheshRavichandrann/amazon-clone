2391e38260266afd6d1f4d39b5e5d86b8a4a2396
Mathesh <mathesh1907@gmail.com>
2025-02-26 16:03:36 +0530

    delivery option interactive

--- data/cart.js
+++ data/cart.js
@@ -9,7 +9,7 @@ if (!cart) {
   {
     productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
     quantity:1,
-     deliveryOptionId: '1'
+     deliveryOptionId: '2'
   }];
 }
 
@@ -51,4 +51,18 @@ export function removeFromCart(productId){
 
   saveToStorage();
 
+}
+
+
+export function updateDeliveryOptions(productId, deliveryOptionId){
+  let matchingItem;
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
+
+  matchingItem.deliveryOptionId = deliveryOptionId;
+
+  saveToStorage();  
 }
