2378d69c1b210baf5c483ae87f461a2df26df7b8
Mathesh <mathesh1907@gmail.com>
2025-02-28 17:22:09 +0530

    add size chart

--- data/products.js
+++ data/products.js
@@ -31,7 +31,31 @@ class Product{
   getPrice(){
     return `$${formatCurrency(this.priceCents)}`;
   }
+
+  extraInfoHTML(){
+    return '';
+  }
 }
+
+
+class Clothing extends Product{
+  sizeChartLink;
+
+  constructor(productDetails){
+    super(productDetails);
+    this.sizeChartLink = productDetails.sizeChartLink;
+  }
+
+  extraInfoHTML(){
+    return `
+    <a href="${this.sizeChartLink}" target="_blank">
+    Size chart
+    </a>
+    `;
+  }
+
+}
+
 
 export const products = [
   {
@@ -693,5 +717,10 @@ export const products = [
     ]
   }
 ].map((productDetails) => {
+  if (productDetails.type === 'clothing') {
+    return new Clothing(productDetails);  
+  }
+
+
   return new Product(productDetails);
 });
